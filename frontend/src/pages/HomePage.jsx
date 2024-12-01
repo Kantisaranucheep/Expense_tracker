b5b413df8ea52d73de7ef5890f21af7172ca3542
Kantisaranucheep <66011015@kmitl.ac.th>
2024-12-01 21:05:18 +0700

    features

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,12 +11,47 @@ function Homepage() {
             <Header />
             <main>
                 <div className="content">
-                    <div className="text-content">
-                        <h2>Take the pain out of creating expense reports</h2>
-                        <p>Manage your expense reporting process easier with Expense Tracker's convenient features and automation tools.</p>
-                        <button>Try it now!</button>
+                    <div className="text-image-container">
+                            <div className="text-content">
+                                <h2>Take the pain out of creating expense reports</h2>
+                                <p>Manage your expense reporting process easier with Expense Tracker's convenient features and automation tools.</p>
+                                <button>Try it now!</button>
+                            </div>
+                            <img src="/assets/images/homepage-pic1.png" alt="Expense Tracker Preview" className="main-image" />
                     </div>
-                    <img src="/assets/images/homepage-pic1.png" alt="Expense Tracker Preview" className="main-image" />
+                        <h2 className="feature-label">Features</h2>
+                    <div className="features-grid">
+                        <div className="feature-item">
+                            <img src="" alt="" />
+                            <h3>Saving plan</h3>
+                            <p>Keep track on savings process to meet your financial goals.</p>
+                        </div>
+                        <div className="feature-item">
+                            <img src="" alt="" />
+                            <h3>Debt and loan</h3>
+                            <p>Manage your debts, loans and payment process in one place.</p>
+                        </div>
+                        <div className="feature-item">
+                            <img src="" alt="" />
+                            <h3>Travel mode</h3>
+                            <p>All currencies supported with up-to-date exchange rates.</p>
+                        </div>
+                        <div className="feature-item">
+                            <img src="" alt="" />
+                            <h3>Travel mode</h3>
+                            <p>All currencies supported with up-to-date exchange rates.</p>
+                        </div>
+                        <div className="feature-item">
+                            <img src="" alt="" />
+                            <h3>Travel mode</h3>
+                            <p>All currencies supported with up-to-date exchange rates.</p>
+                        </div>
+                        <div className="feature-item">
+                            <img src="" alt="" />
+                            <h3>Travel mode</h3>
+                            <p>All currencies supported with up-to-date exchange rates.</p>
+                        </div>
+                    </div>
                 </div>
             </main>
             <Footer />
